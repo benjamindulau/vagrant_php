48e037a4faa63aed4d156f362d61012df512280a
Benjamin Dulau <benjamin.dulau@anonymation.com>
2012-10-16 19:20:16 +0200

    Added mysql-client package dependency for php5-mysql

--- puppet/modules/php5/manifests/mysql.pp
+++ puppet/modules/php5/manifests/mysql.pp
@@ -1,4 +1,9 @@
 class php5::mysql {
+    package { ["mysql-client"]:
+        ensure => latest,
+        require => Exec["aptGetUpdate"],
+    }
+
     package { ["php5-mysql"]:
         ensure => latest,
         require => Package["libapache2-mod-php5", "mysql-client"],
